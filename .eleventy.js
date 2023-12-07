ae5876bb1c6e964d894d2baf2d0790bdd7882da3
Jen Patchett <29377936+JenPatchett@users.noreply.github.com>
2023-12-07 13:01:52 +0000

    Update config to watch css dir

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function (eleventyConfig) {
 
+  // Tells 11ty to pass css, favicons & images to final build:
   eleventyConfig.addPassthroughCopy("./src/styles.css");
+
+  // Tells 11ty dev server to watch css dir & reload site if files change:
+  eleventyConfig.addWatchTarget("./src/css/");
 
   // Set custom directories for input, output, includes, and data
   return {
